d21642753497b79c5cf8883a26ddd6594258059b
Ankit <ankgpt95605@gmail.com>
2026-07-10 12:35:45 +0530

    Update portfolio projects and deployment links

--- components/ui/Home/Hero/Hero.tsx
+++ components/ui/Home/Hero/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
         {/*buttons*/}
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button size={"lg"} asChild className="w-fit mx-auto sm:mx-0">
-                <a href="#_">
+                <a href="https://github.com/Anksyngit" target="_blank" rel="noopener noreferrer">
                     <FolderOpen className="w-5 h-5 mr-2"/>
                     View Projects
                 </a>
